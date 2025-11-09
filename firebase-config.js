d70a4737ed61c53965b53b3884865e6e0afb0743
Ahmed Backer <ahmedbackerjr@gmail.com>
2025-11-09 16:23:15 +0000

    save the world

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Firebase configuration (replace with your actual project values if needed)
 export const firebaseConfig = {
   apiKey: "AIzaSyB8mZzYCujtcG5RylvCx18e6BRVp26kcFY",
   authDomain: "studio-617595928-cae03.firebaseapp.com",
