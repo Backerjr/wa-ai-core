db7529450ccfb759388f1fb1e64ec4360277a485
Ahmed Backer <ahmedbackerjr@gmail.com>
2025-11-09 15:50:57 +0000

    Fix firebase config export

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Firebase configuration (replace with your actual project values if needed)
+export const firebaseConfig = {
   apiKey: "AIzaSyB8mZzYCujtcG5RylvCx18e6BRVp26kcFY",
   authDomain: "studio-617595928-cae03.firebaseapp.com",
   projectId: "studio-617595928-cae03",
@@ -12,6 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "201908464871",
   appId: "1:201908464871:web:e815d2a1cc1284225e4642"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
