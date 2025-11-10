7f1d247d4607c07a2dd9023dadc365459ff3c783
Ahmed Backer <ahmedbackerjr@gmail.com>
2025-11-10 22:15:29 +0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "YOUR-ACTUAL-API-KEY",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "YOUR-SENDER-ID",
+  appId: "YOUR-APP-ID"
+};
