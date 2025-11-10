d0d909c3a1fa0746a0b42abf13aace4eaf59773a
Ahmed Backer <ahmedbackerjr@gmail.com>
2025-11-10 18:43:44 +0000

    Fix: Correct Firebase config loading and restore template

--- firebase-config.template.js
+++ firebase-config.template.js
@@ -14,20 +14,3 @@ export const firebaseConfig = {
   messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
   appId: "REPLACE_WITH_YOUR_APP_ID"
 };
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB8mZzYCujtcG5RylvCx18e6BRVp26kcFY",
-  authDomain: "studio-617595928-cae03.firebaseapp.com",
-  projectId: "studio-617595928-cae03",
-  storageBucket: "studio-617595928-cae03.firebasestorage.app",
-  messagingSenderId: "201908464871",
-  appId: "1:201908464871:web:e815d2a1cc1284225e4642"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
